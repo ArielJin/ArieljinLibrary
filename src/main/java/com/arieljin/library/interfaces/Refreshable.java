81117c430800a6aef6b90c233f9c18fc49ad8249
ArielJin <jiasheng.jin@tom.com>
2018-07-23 18:38:22 +0800

    load more task

---
+++ src/main/java/com/arieljin/library/interfaces/Refreshable.java
@@ -0,0 +1,10 @@
+package com.arieljin.library.interfaces;
+
+/**
+ * @time 2018/7/23.
+ * @email ariel.jin@tom.com
+ */
+public interface Refreshable {
+
+
+}
---
+++ src/main/java/com/arieljin/library/task/RefreshListTask.java
@@ -0,0 +1,69 @@
+package com.arieljin.library.task;
+
+import android.content.Context;
+
+import com.arieljin.library.abs.AbsRequest;
+import com.arieljin.library.listener.OnTaskCompleteListener;
+
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @time 2018/7/23.
+ * @email ariel.jin@tom.com
+ */
+public abstract class RefreshListTask<T extends Serializable> extends RefreshBaseTask<ArrayList<T>> implements OnTaskCompleteListener<ArrayList<T>> {
+
+    @Override
+    protected ArrayList<T> praseJson(JSONObject json) throws Throwable {
+        return null;
+    }
+
+    public RefreshListTask(Context context, AbsRequest request) {
+        super(context, request);
+        addListener(this);
+
+    }
+
+    public RefreshListTask(Context context, AbsRequest request, OnTaskCompleteListener<ArrayList<T>> completeListener) {
+        super(context, request, completeListener);
+    }
+
+    public RefreshListTask(Context context, AbsRequest request, boolean needToast, OnTaskCompleteListener<ArrayList<T>> completeListener) {
+        super(context, request, needToast, completeListener);
+    }
+
+    @Override
+    protected String getRequesturl() {
+        return null;
+    }
+
+    @Override
+    protected HashMap<String, String> addHeaders() {
+        return null;
+    }
+
+
+    @Override
+    public void onTaskComplete(ArrayList<T> result) {
+
+    }
+
+    @Override
+    public void onTaskLoadMoreComplete(ArrayList<T> result) {
+
+    }
+
+    @Override
+    public void onTaskFailed(String error) {
+
+    }
+
+    @Override
+    protected String getApiMethodName() {
+        return null;
+    }
+}
